d580a47aa8018c07d8bed3179f24a867a8a4d1d7
yangkunlin <ainaoteng@foxmail.com>
2021-03-26 09:42:07 +0800

    ConfigMaster commit

---
+++ ms-util-dynamic-datasource/src/main/java/sevencode/util/dynamic/datasource/config/ConfigMaster.java
@@ -0,0 +1,12 @@
+package sevencode.util.dynamic.datasource.config;
+
+/***********************************************
+ * @Author: Seven
+ * @Date: 2021/3/26 9:36 上午
+ * @Description:
+ * @Modified:
+ * @Version: 1.0.0
+ * @Copyright 2020-2021 Comleader - Powered By 凌鉴项目组
+ **********************************************/
+public class ConfigMaster {
+}
