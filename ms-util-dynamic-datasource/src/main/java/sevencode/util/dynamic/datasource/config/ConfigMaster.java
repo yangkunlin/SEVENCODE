a54273ebea50a54f0b0f3f2f98ec6043cae65355
yangkunlin <ainaoteng@foxmail.com>
2021-03-26 09:52:37 +0800

    ConfigMaster

--- ms-util-dynamic-datasource/src/main/java/sevencode/util/dynamic/datasource/config/ConfigMaster.java
+++ ms-util-dynamic-datasource/src/main/java/sevencode/util/dynamic/datasource/config/ConfigMaster.java
@@ -1,5 +1,9 @@
 package sevencode.util.dynamic.datasource.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
 /***********************************************
  * @Author: Seven
  * @Date: 2021/3/26 9:36 上午
@@ -8,5 +12,16 @@ package sevencode.util.dynamic.datasource.config;
  * @Version: 1.0.0
  * @Copyright 2020-2021 Comleader - Powered By 凌鉴项目组
  **********************************************/
+@Configuration
+@Data
 public class ConfigMaster {
+    @Value("${spring.datasource.dynamic.datasource.master.url}")
+    private String url;
+    @Value("${spring.datasource.dynamic.datasource.master.username}")
+    private String username;
+    @Value("${spring.datasource.dynamic.datasource.master.password}")
+    private String password;
+    @Value("${spring.datasource.dynamic.datasource.master.driver-class-name}")
+    private String driverClassName;
+
 }
